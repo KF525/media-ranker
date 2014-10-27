aee9846de3ab4b4e0753d1215026a0b535f62010
Kate Fulton <katesuzannefulton@gmail.com>
2014-10-27 16:55:19 -0700

    Add books functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,56 @@
 class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(params.require(:book).permit(:name, :description, :author))
+    @book.rank = 0
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(params.require(:book).permit(:name, :description, :author))
+      redirect_to show_book_path
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to books_path
+    else
+      redirect_to show_book_path
+    end
+  end
+
+  def upvote
+    @book = Book.find(params[:id])
+    @book.rank += 1
+    if @book.save
+      redirect_to books_path
+    else
+      redirect_to show_book_path
+    end
+  end
 end
